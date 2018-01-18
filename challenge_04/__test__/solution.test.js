7dcf7ee5a4238d01a1067752e7963f07fa0ed9be
Kevin Miller <kevin@driftabout.com>
2018-01-18 14:58:45 -0800

    finshed tests

--- challenge_04/__test__/solution.test.js
+++ challenge_04/__test__/solution.test.js
@@ -9,10 +9,29 @@ describe('solution', () => {
   test('Test 2: solution.intersect is a function', () => {
     expect(solution.intersect).toBeInstanceOf(Function);
   });
-  /*
-  test('Test 3: intersect returns null when the argument is not an object', () => {
-    expect(solution.intersect(['kevin'])).toBe(null);
+  
+  test('Test 3: intersect returns null when there is less than 2 arguments', () => {
+    expect(solution.intersect([1,2])).toBeNull();
   });
+
+  test('Test 4: intersect returns null when either argument is not an array', () => {
+    expect(solution.intersect('kevin', [1,2])).toBeNull();
+  });
+
+  test('Test 5: intersect returns null when there is not an intersection', () => {
+    expect(solution.intersect(['kevin'], [1,2])).toBeNull();
+  });
+
+  test('Test 6: intersect returns an array of intersecting items when the items exist in ech array', () => {
+    let total = solution.intersect([1, 2], [1, 2, 3]).reduce((acc, cur) => acc + cur );
+    expect(total).toEqual(3);
+  });
+
+  test('Test 7: intersect returns an array of unique intersections when duplicates are present.', () => {
+    expect(solution.intersect(['kevin', 'kevin'], ['kevin', 'kevin', 'kevin']).toString()).toEqual('kevin');
+  });
+
+  /*
   test('Test 4: intersect returns sum of values correctly', () => {
     let data = {value: 2, next:{value: 3, next: {value: 1, next: null}}};
     expect(solution.intersect(data)).toEqual(6);
--- challenge_04/lib/solution.js
+++ challenge_04/lib/solution.js
@@ -5,7 +5,8 @@ const solution = module.exports = {};
 const intersect = (...arr) => {
   if (arr.length !== 2 || !arr.every(val => Array.isArray(val))) return null;
   let [short, long ] = arr.sort((a,b) => a.length < b.length ? - 1 : 1 );
-  return short.filter(val => long.includes(val)); 
+  let cross = short.filter(val => long.includes(val));
+  return cross.length ? [...new Set(cross)] : null; 
 };
 
 solution.intersect = intersect;
