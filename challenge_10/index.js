e548ee799a457cd7231c7fecb6fca70c36e00eaa
Kevin Miller <kevin@driftabout.com>
2018-01-26 15:37:26 -0800

    scsaffold complete

--- challenge_10/index.js
+++ challenge_10/index.js
@@ -1,17 +1,10 @@
 'use strict';
 
+const Stack = require('../lib/stack');
+
 const solution = module.exports = {};
 
-solution.findNthNodeFromEnd = (n, lnklst) => {
-  if( typeof n !== 'number' || !lnklst.hasOwnProperty('head')) return null;
-  if(n < 0) return null;
-  let i = 0;
-  let curr_node = lnklst.head;
-  for(let next_node = lnklst.head; next_node.next; next_node = next_node.next){
-    if((i > n)) curr_node = curr_node.next;
-    i++;
-  }
-  curr_node = curr_node.next;
-  if(i < n) return null;
-  return {head: curr_node};
-};
+soultion.checkBraces = (braces_str) => {
+if (typeof braces_str !== 'string') return new Error('Invalid input: Expecting string');
+
+}
---
+++ challenge_10/lib/binary-search.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const binary_search = module.exports = {};
+
