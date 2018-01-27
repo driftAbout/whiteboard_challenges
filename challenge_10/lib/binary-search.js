a7320f188d5c9994ea57483017f5be13a9dd75ec
Kevin Miller <kevin@driftabout.com>
2018-01-26 23:29:34 -0800

    cleaned up binary search

--- challenge_10/lib/binary-search.js
+++ challenge_10/lib/binary-search.js
@@ -7,15 +7,10 @@ module.exports = function(val, dataSet) {
   const half = (max, min) =>  Math.floor((max - min) / 2);
   let min = 0;
   let max = dataSet.length - 1;
-  let mid = Math.floor(max/2);
+  let mid = half(max, min);
   
   let exists = false;
   while (!exists) {
-    debug('VALUE: ', val);
-    debug('min', min, dataSet[min]);
-    debug('mid', mid, dataSet[mid]);
-    debug('max', max, dataSet[max]);
-
     if (val === dataSet[mid]) exists = true;
     if (min === max ) break;
     val > dataSet[mid] ? ( min = ++mid, mid+=half(max, min)) : (max = --mid,  mid-=half(max, min) );
