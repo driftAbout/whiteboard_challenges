19dbb7d34050d589720c57895cffd0bab4a244a7
Kevin Miller <kevin@driftabout.com>
2018-01-26 20:43:24 -0800

    created left and rights

--- challenge_10/lib/binary-search.js
+++ challenge_10/lib/binary-search.js
@@ -4,9 +4,8 @@ const debug = require('debug')('http:binary-search');
 
 module.exports = function(val, dataSet) {
   if (typeof val !== 'number' || ! Array.isArray(dataSet)) throw new Error('Invalid input: Expecting (number, array)');
-  const half = (val) => Math.floor(val / 2);
-  const right = (max, mid) => [max + 1, Math.floor(max - mid / 2) + mid];
-  const left = (mid, min) => [mid - 1, min - (Math.floor(mid - min / 2))];
+  const right = (max, mid) => [max + 1, Math.floor((max - mid) / 2) + mid];
+  const left = (mid, min) => [mid - 1, min - Math.floor((mid - min) / 2)];
   let min = 0;
   let max = dataSet.length - 1;
   let mid = half(max);
@@ -23,10 +22,8 @@ module.exports = function(val, dataSet) {
     debug('max', max, dataSet[max]);
     if (min === max) break;
     if (val === dataSet[mid]) exists = true;
-    //if(val > dataSet[mid]) [min, mid] = [max + 1, half(max - mid) + mid];
     if(val > dataSet[mid]) [min, mid] = right(max, mid);
-    max = mid - 1;
-    mid = mid - half(mid - min);
+    [max, mid] = left(mid, min);
   }
   return exists;
 };
