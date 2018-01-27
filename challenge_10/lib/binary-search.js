8720f496fbb2d39fbea2b904e4750938f5f666ef
Kevin Miller <kevin@driftabout.com>
2018-01-26 23:00:29 -0800

    binary-sesrch working

--- challenge_10/lib/binary-search.js
+++ challenge_10/lib/binary-search.js
@@ -4,26 +4,59 @@ const debug = require('debug')('http:binary-search');
 
 module.exports = function(val, dataSet) {
   if (typeof val !== 'number' || ! Array.isArray(dataSet)) throw new Error('Invalid input: Expecting (number, array)');
-  const right = (max, mid) => [max + 1, Math.floor((max - mid) / 2) + mid];
-  const left = (mid, min) => [mid - 1, min - Math.floor((mid - min) / 2)];
+  const half = (max, min) =>  Math.floor((max - min) / 2);
   let min = 0;
   let max = dataSet.length - 1;
-  let mid = half(max);
+  let mid = Math.floor(max/2);
   
   let exists = false;
-
+  //exists = true;
   let x = 0;
-  //while (!exists) {
-  while (x < 5) {
+  while (!exists) {
+  //w//hile (esits) {
     x++;
     debug('VALUE: ', val);
     debug('min', min, dataSet[min]);
     debug('mid', mid, dataSet[mid]);
     debug('max', max, dataSet[max]);
-    if (min === max) break;
+
     if (val === dataSet[mid]) exists = true;
-    if(val > dataSet[mid]) [min, mid] = right(max, mid);
-    [max, mid] = left(mid, min);
+    if (min === max ) exists = false;
+   // 
+
+   // if (val === dataSet[mid]) exists = true;
+   // if (min === max) break;
+
+    //if(val > dataSet[mid]) [min, mid] = right(min, max, mid);
+  //  if (val < dataSet[mid]) [max, mid] = left(min, max, mid);
+    //if(val > dataSet[mid]) [min, mid] = [++mid, mid+= Math.floor((max - min) / 2)];
+   // if (val < dataSet[mid]) [max, mid] = [--mid, mid-= Math.floor((max - min) / 2)];
+    //if(val > dataSet[mid]) [min, mid] = [max + 1, half(max - mid) + mid];
+    
+     //val > dataSet[mid] ? ( min = ++mid, mid+= Math.floor((max - min) / 2)) : (max = --mid,  mid-= Math.floor((max - min) / 2) );
+
+     val > dataSet[mid] ? ( min = ++mid, mid+=half(max, min)) : (max = --mid,  mid-=half(max, min) );
+     
+    // if(val > dataSet[mid]) {
+    // min = ++mid; 
+    // mid+= Math.floor((max - min) / 2);
+    // }  
+     
+    
+    //   // mid = mid + 1
+    //  // mid = Math.floor((max - min) / 2) + mid ;
+    // } 
+    // if (val < dataSet[mid]) {
+    
+      // if (val < dataSet[mid]){ 
+      //   max = --mid;  
+      //   mid-= Math.floor((max - min) / 2);
+      // }
+  //   // }
+  // //     mid = mid - 1;
+  //   mid = mid - half(mid - min);
+  //   mid = mid - Math.floor((max - min) / 2);
+  //  }
   }
   return exists;
 };
