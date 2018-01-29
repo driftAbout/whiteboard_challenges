934d16942be53eb45e1e1b4620221b45419daafd
Kevin Miller <kevin@driftabout.com>
2018-01-29 14:55:59 -0800

    added solution

--- challenge_11/__test__/index.test.js
+++ challenge_11/__test__/index.test.js
@@ -3,13 +3,47 @@
 const solution = require(`${__dirname}/../index`);
 
 describe('solution', () => {
-  describe('valid input', () => {
-    it('Test 1: solution exists', () => {
+  describe('Exists', () => {
+    it('solution exists', () => {
       expect(solution).toBeDefined();
     });
 
-    it('Test 2: sll exists', () => {
-    // expect( ).toBeDefined();
+    it('solution.calculateMissing should exists', () => {
+      expect( solution.calculateMissing).toBeDefined();
+    });
+
+    it('solution.calculateMissing should exists', () => {
+      expect(solution.calculateMissing).toBeInstanceOf(Function);
+    });
+  });
+  describe('Valid input', () => {
+
+    beforeAll(() => {
+      this.missing = 21;
+      this.arr = [...Array(100)].map((val, i) => i + 1);
+      delete this.arr[this.missing];
+
+    });
+
+    it('should return the missing value from an array', () => {
+      expect(solution.calculateMissing(this.arr)).toEqual(this.missing + 1);
+    });
+
+    it('should return false for arr.includes(missing )', () => {
+      let val = solution.calculateMissing(this.arr);
+      expect(this.arr.includes(val)).toBe('false');
+    });
+
+  });
+  describe('Invalid input', () => {
+    it('should throw invalid input error', () => {
+      expect(() => {
+        let arr = 'array';
+        expect(solution.calculateMissing(arr));
+      }).toThrow('Invalid input:  Expecting array');
+      expect(() => {
+        expect(solution.calculateMissing());
+      }).toThrow('Invalid input:  Expecting array');
     });
   });
 });
