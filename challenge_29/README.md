013e84854de0a930e776b8f527e659888c82e857
Kevin Miller <kevin@driftabout.com>
2018-02-22 16:08:20 -0800

    added readme

--- challenge_29/README.md
+++ challenge_29/README.md
@@ -18,13 +18,46 @@
  
   ## Solution
 
+  - Validate input, return errors for non arrays and array of non strings
+  
+  - Iterate on each word on the array
+
+    - split words into letters
+
+    - sort letters alphabetically
+
+    - join letters
+  
+    - create key in object, if not exists set initial value to empty array 
+    
+    - push word to array at letters key
+
+  - Get array of values from object
+
+  - Filter array to remove arrays with only one valiue
+
+  - Create new object with each array as value and item at index 0 as key
+
 
   ## Tests
 
   ### Valid input
+
+  - Validate the return of an object of anagrams with expected keys
      
+  - Validate the return of an object of anagrams with expected array of words
+
+  - Validate that the object of anagrams does not contain a property for words that have no anagrams
+
+  - Validate the return of null for arrays that have no anagrams.
 
   ### Invalid input
 
+  - Validate the return of an error for input values that are not arrays
+
+  - Validate the return of an error for input arrays that contain data other than strings
+
+
+     
    
     
--- challenge_29/__test__/index.test.js
+++ challenge_29/__test__/index.test.js
@@ -8,10 +8,33 @@ describe('solution', () => {
       expect(solution).toBeDefined();
     });
 
-    this.arr = ['acre', 'race', 'care', 'act', 'cat', 'tac'];
+    this.arr = ['acre', 'race', 'care', 'act', 'cat', 'tac', 'pat'];
     this.anagrams = solution.anagram_sort(this.arr);
-    it('Should return object of anagrams', () => {
+    it('Should return object of anagrams with keys', () => {
       expect(Object.keys(this.anagrams)).toEqual(expect.arrayContaining(['acre', 'act']));
+    });
+
+    it('Should return object of anagrams with keys of words', () => {
+      expect(this.anagrams.acre).toEqual(expect.arrayContaining(['acre', 'race', 'care']));
+    });
+
+    it('Should not contain a property for words that have no anagrams', () => {
+      expect(this.anagrams.pat).toBeUndefined();
+    });
+
+    it('Should return null for arrays that have no anagrams', () => {
+      expect(solution.anagram_sort(['hello', 'goodbye'])).toBeNull();
+    });
+  });
+
+
+  
+  describe('invalid input', () => {
+    it('Should return an error for input values that are not arrays', () => {
+      expect(() => solution.anagram_sort('Hello')).toThrow();
+    });
+    it('Should return an error for input arrays that contain data other than strings', () => {
+      expect(() => solution.anagram_sort(['R2D2', 'C3PO', 'rumba', 'Rosie'])).toThrow();
     });
   });
 });
